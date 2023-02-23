16c1c02909b171a1bbc41010e0ee2e5654e493d6
Asdrubalini <asdrubalini@mail.com>
2023-02-23 18:55:09 +0100

    use fuse-t on macos

--- fuse-sys/build.rs
+++ fuse-sys/build.rs
@@ -2,11 +2,17 @@
 const LIBFUSE_NAME: &str = "fuse";
 
 #[cfg(target_os = "macos")]
-const LIBFUSE_NAME: &str = "osxfuse";
+const LIBFUSE_NAME: &str = "fuse-t";
 
 fn main() {
+    let version = if cfg!(target_os = "macos") {
+        "1.0"
+    } else {
+        "2.6.0"
+    };
+
     pkg_config::Config::new()
-        .atleast_version("2.6.0")
+        .atleast_version(version)
         .probe(LIBFUSE_NAME)
         .map_err(|e| eprintln!("{}", e))
         .unwrap();
