26f14a4289862a6cd0c5b481c51a3c8390151d4a
Asdrubalini <asdrubalini@mail.com>
2023-03-04 10:36:52 +0100

    switch to the new version of macfuse

--- fuse-sys/build.rs
+++ fuse-sys/build.rs
@@ -1,18 +1,8 @@
-#[cfg(not(target_os = "macos"))]
 const LIBFUSE_NAME: &str = "fuse";
 
-#[cfg(target_os = "macos")]
-const LIBFUSE_NAME: &str = "fuse-t";
-
 fn main() {
-    let version = if cfg!(target_os = "macos") {
-        "1.0"
-    } else {
-        "2.6.0"
-    };
-
     pkg_config::Config::new()
-        .atleast_version(version)
+        .atleast_version("2.6.0")
         .probe(LIBFUSE_NAME)
         .map_err(|e| eprintln!("{}", e))
         .unwrap();
